f677ca4bbc28458ba1ca41a49f673db089412762
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-10-14 12:38:01 +0000

    Adding home-styles

--- assets/js/home-scroll.js
+++ assets/js/home-scroll.js
@@ -1,3 +1,7 @@
+const logo = document.getElementById('logo');
+const navbar = document.getElementById('nav');
+const sec1 = document.getElementById('sec1');
+
 const home = document.getElementById('home');
 const about = document.getElementById('about');
 const contact = document.getElementById('contact');
@@ -8,6 +12,14 @@ window.onscroll = function() {
     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
 
     if(scrollPosition >= 200){
+        logo.classList.add('par-logo');
+        logo.classList.remove('logo');
+        navbar.classList.add('par-nav');
+        navbar.classList.remove('nav');
+        sec1.classList.add('par-sec1');
+        sec1.classList.remove('sec1');
+
+
         home.classList.add('par-nav-link-style-active');
         about.classList.add('par-nav-link-style');
         contact.classList.add('par-nav-link-style');
@@ -22,6 +34,13 @@ window.onscroll = function() {
     }
 
     if(scrollPosition <= 199){
+        logo.classList.add("logo");
+        logo.classList.remove("par-logo");
+        navbar.classList.add('nav');
+        navbar.classList.remove('par-nav');
+        sec1.classList.add('sec1');
+        sec1.classList.remove('par-sec1');
+
         home.classList.remove('par-nav-link-style-active');
         about.classList.remove('par-nav-link-style');
         contact.classList.remove('par-nav-link-style');
